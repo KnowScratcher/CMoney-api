47a5a87c5064a7ba84caa0e8bb953f65a7b9e667
KnowScratcher <yianlee2008@gmail.com>
2024-06-10 15:13:49 +0800

    build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 
 HERE = pathlib.Path(__file__).parent.resolve()
 
-PACKAGE_NAME = "CMoney API"
+PACKAGE_NAME = "cmoney-api"
 AUTHOR = "Know Scratcher"
 AUTHOR_EMAIL = "yianlee2008@gmail.com"
 URL = "https://github.com/KnowScratcher/CMoney-api"
@@ -14,3 +14,30 @@ VERSION = "1.0.0"
 DESCRIPTION = "一個股票大富翁的操作API"
 LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf8")
 LONG_DESC_TYPE = "text/markdown"
+
+requirements = (HERE / "requirements.txt").read_text(encoding="utf8")
+INSTALL_REQUIRES = [s.strip() for s in requirements.split("\n")]
+ 
+# dev_requirements = (HERE / "dev_requirements.txt").read_text(encoding="utf8")
+# EXTRAS_REQUIRE = {"dev": [s.strip() for s in dev_requirements.split("\n")]}
+ 
+CLASSIFIERS = [f"Programming Language :: Python :: 3.{str(v)}" for v in range(7, 12)]
+PYTHON_REQUIRES = ">=3.7"
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    license=LICENSE,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    # extras_require=EXTRAS_REQUIRE,
+    packages=find_packages(),
+    classifiers=CLASSIFIERS,
+)
